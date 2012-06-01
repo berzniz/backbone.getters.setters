4c7bad34cb4c299465c99fee46b8331ad88b80f8
Colin Thomas-Arnold <colinta@gmail.com>
2012-05-31 19:07:56 -0600

    formatters, and a fix

--- backbone.getters.setters.js
+++ backbone.getters.setters.js
@@ -1,36 +1,57 @@
+// from https://github.com/berzniz/backbone.getters.setters
 Backbone.GSModel = Backbone.Model.extend({
 
-	get: function(attr) {
-		// Call the getter if available
-		if (_.isFunction(this.getters[attr])) {
-			return this.getters[attr].call(this);
-		}
-		
-		return Backbone.Model.prototype.get.call(this, attr);
-	},
+  get: function(attr) {
+    var format = arguments.length > 1 ? arguments[1] : null;
+    // Call the getter if available
+    if (_.isFunction(this.getters[attr])) {
+      return this.getters[attr].call(this, format);
+    }
 
-	set: function(key, value, options) {
-		// Normalize the key-value into an object
-		if (_.isObject(key) || key == null) {
-			attrs = key;
-			options = value;
-		} else {
-			attrs = {};
-			attrs[key] = value;
-		}
+    return Backbone.Model.prototype.get.call(this, attr);
+  },
 
-		// Go over all the set attributes and call the setter if available
-		for (attr in attrs) {
-			if (_.isFunction(this.setters[attr])) {
-				attrs[attr] = this.setters[attr].call(this, attrs[attr]);
-			}
-		}
+  set: function(key, value, format, options) {
 
-		return Backbone.Model.prototype.set.call(this, attrs, options);
-	},
-	
-	getters: {},
-	
-	setters: {}
-	
+    // Normalize the key-value into an object
+    if (_.isObject(key) || key == null) {
+      attrs = key;
+      format = value;
+      options = format;
+    } else {
+      attrs = {};
+      attrs[key] = value;
+    }
+
+    if ( _.isObject(format) )
+    {
+      options = format;
+      format = null;
+    }
+
+    // Go over all the set attributes and call the setter if available
+    for (var attr in attrs) {
+      if ( ! attrs.hasOwnProperty(attr) )
+        continue;
+      if (_.isFunction(this.setters[attr])) {
+        attrs[attr] = this.setters[attr].call(this, attrs[attr], format);
+      }
+    }
+
+    return Backbone.Model.prototype.set.call(this, attrs, options);
+  },
+
+  rawget: function() {
+    return Backbone.Model.prototype.get.apply(this, arguments);
+  },
+
+  rawset: function() {
+    return Backbone.Model.prototype.set.apply(this, arguments);
+  },
+
+
+  getters: {},
+
+  setters: {}
+
 });
